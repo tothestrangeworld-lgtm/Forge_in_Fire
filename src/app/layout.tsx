4a6db6c6d60220820d08d0a85470b837ed9098b9
Yoshiki <to.the.strange.world@gmail.com>
2026-05-02 21:47:39 +0900

    アイコン画像修正２

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/icon/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icon/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/icon/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
     ],
   },
   other: {
